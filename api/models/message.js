81b53953b294f997537dd59c7685242de9cdc934
DDobrowolski <damian.dobrowolski99@gmail.com>
2018-10-31 23:03:52 +0100

    Initial commit MesSend

---
+++ api/models/message.js
@@ -0,0 +1,20 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var MessageSchema = new Schema({
+    author: {
+      type: String,
+      required: 'There is no message author'
+    },
+    date: {
+      type: Date,
+      default: Date.now
+    },
+    content: {
+        type: String,
+      required: 'Message cannot be empty'
+    }
+  });
+  
+  module.exports = mongoose.model('Tasks', TaskSchema);
