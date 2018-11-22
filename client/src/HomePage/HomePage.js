dbe17900084f16255b81f9c1729a1f6e833f36e5
damian.dobrowolski99@gmail.com <damian.dobrowolski99@gmail.com>
2018-11-22 22:59:31 +0100

    Added conditional rendering

--- client/src/HomePage/HomePage.js
+++ client/src/HomePage/HomePage.js
@@ -3,19 +3,25 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './HomePage.scss';
 import userImage from './img/iconfinder_user-group_285648.png';
 export default class HomePage extends Component {
+  remember = false;
+  actualUser = 'Test user';
+  logIn() {
+    alert('Elo')
+  }
   loginForms = (
-    <form>
+    <form onSubmit={this.logIn}>
     <div class="form-group">
-      <label for="exampleInputEmail1">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+      <label for="email">Email address</label>
+      {!this.remember &&<input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"/>}
+      {this.remember && <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder={this.actualUser} readOnly/>}
     </div>
     <div class="form-group">
-      <label for="exampleInputPassword1">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+      <label for="password">Password</label>
+      <input type="password" class="form-control" id="password" placeholder="Password"/>
     </div>
     <div class="form-check">
-      <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-      <label class="remember-me" for="exampleCheck1">Remember me</label>
+      <input type="checkbox" class="form-check-input" />
+      <label class="remember-me" for="rememberMe">Remember me</label>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
@@ -24,7 +30,7 @@ export default class HomePage extends Component {
   loginCard = (
     <div class="card text-center" style = {{width: "18rem"}}>
     <div class="card-header">
-    Welcome!
+    {!this.remember && 'Welcome!'} {this.remember && `Welcome back ${this.actualUser}`}
     </div>
     <img class="card-img-top" src={userImage} alt="User"/>
     <div class="card-body">
