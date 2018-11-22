c13b51531eca2821c6e0653488203b7b34e20b8b
damian.dobrowolski99@gmail.com <damian.dobrowolski99@gmail.com>
2018-11-22 22:28:10 +0100

    Added login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import HomePage from './HomePage/HomePage';
+
 
 class App extends Component {
   render() {
-    return (<HomePage></HomePage>);
+    return (<HomePage ></HomePage>);
   }
 }
 export default App;
--- client/src/HomePage/HomePage.js
+++ client/src/HomePage/HomePage.js
@@ -1,8 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import './HomePage.scss';
+import userImage from './img/iconfinder_user-group_285648.png';
+export default class HomePage extends Component {
+  loginForms = (
+    <form>
+    <div class="form-group">
+      <label for="exampleInputEmail1">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+    </div>
+    <div class="form-group">
+      <label for="exampleInputPassword1">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+    </div>
+    <div class="form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+      <label class="remember-me" for="exampleCheck1">Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+  )
 
-class HomePage extends Component {
-render () {
-    return (<div>HomePage</div>)
+  loginCard = (
+    <div class="card text-center" style = {{width: "18rem"}}>
+    <div class="card-header">
+    Welcome!
+    </div>
+    <img class="card-img-top" src={userImage} alt="User"/>
+    <div class="card-body">
+    <h5 class="card-title">Please log in:</h5>
+    {this.loginForms}
+  </div>
+    </div>
+  )
+  content = (<div class="container" >{this.loginCard}</div>)
+
+  render() {
+    return this.content;
+  }
 }
-}
-export default HomePage;
