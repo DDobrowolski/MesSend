5610cf094e224285046f87164b67b59321373e6c
damian.dobrowolski99@gmail.com <damian.dobrowolski99@gmail.com>
2018-11-26 23:20:43 +0100

    Added navbar

---
+++ client/src/Board/Board.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import NavBar from '../NavBar/Navbar';
+
+
+class Board extends Component {
+    render() {
+      return (new NavBar());
+    }
+  }
+  export default Board;
+  
--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import Board from './Board/Board';
 import * as serviceWorker from './serviceWorker';
+import { Route } from 'react-router'
+import {BrowserRouter} from 'react-router-dom';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+    <BrowserRouter>
+    <div>
+      <Route exact path='/' component={App} />
+      <Route path='/board' component={Board} />
+    </div>
+  </BrowserRouter>
+  ), document.getElementById('root'));
 
 serviceWorker.unregister();
