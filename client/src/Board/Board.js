cb2ade918ded4e4ecb2b396ec458ed0988236d2c
damian.dobrowolski99@gmail.com <damian.dobrowolski99@gmail.com>
2018-11-26 23:56:34 +0100

    Added board.

--- client/src/Board/Board.js
+++ client/src/Board/Board.js
@@ -1,11 +1,22 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import NavBar from '../NavBar/Navbar';
+import './Board.scss';
 
+const content = (
+  <div class="container">
+<div class="post">
+<div class="row">
+<div class="col">
+  One of three columns
+  </div>
+  </div>
+  </div>
+</div>)
 
 class Board extends Component {
     render() {
-      return (new NavBar());
+      return ([new NavBar(), content]);
     }
   }
   export default Board;
