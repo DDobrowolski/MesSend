df6a8cac5ffb7a239df80d4d754aeead5c86e24d
DDobrowolski <damian.dobrowolski99@gmail.com>
2018-12-07 17:48:45 +0100

    Added managing userdata

--- client/src/Profile/Profile.js
+++ client/src/Profile/Profile.js
@@ -5,17 +5,22 @@ import './Profile.scss';
 import axios from 'axios';
 
 const getUserData = (id) => {
-  const username = axios.get(`http://localhost:8080/users/${id}`).then(res =>  res.data.username);
-
+  return axios.get(`http://localhost:8080/users/${id}`).then(res => res.data.username);
+  
 }
 
 class Profile extends Component {
-  state = {messages: []}
+  state = {userdata: {}}
   componentDidMount() {
+  getUserData(this.props.match.params.id).then(data => {
+    this.setState({userdata: {username: data}});
+    console.log(data)
+  })
+   
   }
 
     render() {
-      return ([new NavBar()]);
+      return ([new NavBar(), <div>{this.state.userdata.username}</div>]);
     }
   }
   export default Profile;
