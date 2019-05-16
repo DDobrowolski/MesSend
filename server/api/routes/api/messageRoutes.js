d91e8a3b4399aac1a102e27e3d4d2ddd93a0ceeb
DDobrowolski <damian.dobrowolski99@gmail.com>
2019-05-17 00:28:09 +0200

    Refactored controllers

--- server/api/routes/api/messageRoutes.js
+++ server/api/routes/api/messageRoutes.js
@@ -1,21 +1,21 @@
 'use strict';
 const router = require('express').Router();
-const mesSend = require('../../controllers/messageController');
+const messageController = require('../../controllers/messageController');
 
 router.route('/')
-  .get(mesSend.get_all_messages)
-  .post(mesSend.create_message);
+  .get(messageController.get_all_messages)
+  .post(messageController.create_message);
 
 
 router.route('/:messageId')
-  .get(mesSend.get_one_message)
-  .put(mesSend.update_message)
-  .delete(mesSend.delete_message);
+  .get(messageController.get_one_message)
+  .put(messageController.update_message)
+  .delete(messageController.delete_message);
 
 router.route('/:messageId/replies')
-  .post(mesSend.add_reply);
+  .post(messageController.add_reply);
 
 router.route('/author/:authorId')
-  .get(mesSend.get_author_messages);
+  .get(messageController.get_author_messages);
 
 module.exports = router;
--- server/api/routes/api/userRoutes.js
+++ server/api/routes/api/userRoutes.js
@@ -1,19 +1,19 @@
 'use strict';
 const router = require('express').Router();
 
-var mesSend = require('../../controllers/userController');
+const userController = require('../../controllers/userController');
 
 router.route('/')
-  .get(mesSend.get_all_users)
-  .post(mesSend.create_user);
+  .get(userController.get_all_users)
+  .post(userController.create_user);
 
 
 router.route('/:userId')
-  .get(mesSend.get_one_user)
-  .put(mesSend.update_user)
-  .delete(mesSend.delete_user);
+  .get(userController.get_one_user)
+  .put(userController.update_user)
+  .delete(userController.delete_user);
 
 router.route('/:userId/messages')
-  .get(mesSend.get_user_messages);
+  .get(userController.get_user_messages);
 
 module.exports = router;
