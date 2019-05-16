547131a567eacf718c48e90871a546ad980d2d4a
DDobrowolski <damian.dobrowolski99@gmail.com>
2019-05-17 00:31:34 +0200

    Added auth routes and controller methods

--- server/api/controllers/userController.js
+++ server/api/controllers/userController.js
@@ -1,13 +1,12 @@
 'use strict';
 
-
 const mongoose = require('mongoose'),
-  User = mongoose.model('Users');
+  User = mongoose.model('Users'),
+  passport = require('passport');
 
 exports.get_all_users = function (req, res) {
   User.find({}, function (err, user) {
-    if (err)
-      res.send(err);
+    if (err) res.send(err);
     res.json(user);
   });
 };
@@ -17,50 +16,113 @@ exports.create_user = function (req, res) {
   new_user.setPassword(req.body.password);
 
   new_user.save(function (err, user) {
-    if (err)
-      res.send(err);
-    console.log(`Created user ${user}`)
+    if (err) res.send(err);
+    console.log(`Created user ${user}`);
     res.json(user);
   });
 };
 
 exports.get_one_user = function (req, res) {
   User.findById(req.params.userId, function (err, user) {
-    if (err)
-      res.send(err);
+    if (err) res.send(err);
     res.json(user);
   });
 };
 
 exports.update_user = function (req, res) {
   User.findOneAndUpdate({
-    _id: req.params.userId
-  }, req.body, {
-    new: true
-  }, function (err, user) {
-    if (err)
-      res.send(err);
-    res.json(user);
-  });
+      _id: req.params.userId
+    },
+    req.body, {
+      new: true
+    },
+    function (err, user) {
+      if (err) res.send(err);
+      res.json(user);
+    }
+  );
 };
 
 exports.delete_user = function (req, res) {
   User.remove({
-    _id: req.params.userId
-  }, function (err, user) {
-    if (err)
-      res.send(err);
-    console.log(`Deleted user ${user}`)
-    res.json({
-      user: 'User successfully deleted'
-    });
-  });
+      _id: req.params.userId
+    },
+    function (err, user) {
+      if (err) res.send(err);
+      console.log(`Deleted user ${user}`);
+      res.json({
+        user: 'User successfully deleted'
+      });
+    }
+  );
 };
 
 exports.get_user_messages = function (req, res) {
   User.findById(req.params.userId, function (err, user) {
-    if (err)
-      res.send(err);
-    res.json(user);
-  }).populate('messages').exec();;
+      if (err) res.send(err);
+      res.json(user);
+    })
+    .populate('messages')
+    .exec();
 };
+
+exports.sign_in = (req, res, next) => {
+  const {
+    body: {
+      user
+    }
+  } = req;
+
+  if (!user.email) {
+    return res.status(422).json({
+      errors: {
+        email: 'is required'
+      }
+    });
+  }
+
+  if (!user.password) {
+    return res.status(422).json({
+      errors: {
+        password: 'is required'
+      }
+    });
+  }
+
+  return passport.authenticate(
+    'local', {
+      session: false
+    },
+    (err, passportUser, info) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (passportUser) {
+        const user = passportUser;
+        user.token = passportUser.generateJWT();
+
+        return res.json({
+          user: user.toAuthJSON()
+        });
+      }
+
+      return status(400).info;
+    }
+  )(req, res, next);
+};
+
+exports.get_current = (req, res, next) => {
+  const {
+    payload: {
+      id
+    }
+  } = req;
+
+  return User.findById(id, (err, user) => {
+    if (err) return res.send(err);
+    return res.json({
+      user: user.toAuthJSON()
+    });
+  });
+}
--- server/api/routes/api/userRoutes.js
+++ server/api/routes/api/userRoutes.js
@@ -1,19 +1,26 @@
 'use strict';
 const router = require('express').Router();
+const auth = require('../auth');
 
-const userController = require('../../controllers/userController');
+const user_controller = require('../../controllers/userController');
 
 router.route('/')
-  .get(userController.get_all_users)
-  .post(userController.create_user);
+  .get(user_controller.get_all_users)
+  .post(user_controller.create_user);
 
 
 router.route('/:userId')
-  .get(userController.get_one_user)
-  .put(userController.update_user)
-  .delete(userController.delete_user);
+  .get(user_controller.get_one_user)
+  .put(user_controller.update_user)
+  .delete(user_controller.delete_user);
 
 router.route('/:userId/messages')
-  .get(userController.get_user_messages);
+  .get(user_controller.get_user_messages);
+
+router.route('/sign_in')
+  .post(auth.optional, user_controller.sign_in(req, res, next));
+
+router.route('/current')
+  .get(auth.required, user_controller.get_current(req, res, next));
 
 module.exports = router;
